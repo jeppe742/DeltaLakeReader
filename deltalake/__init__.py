2647bf83dc05be420fa66bdcd71865edbcb0568e
Jeppe Johan Waarkjær Olsen <jeppe742@gmail.com>
2020-12-26 12:37:40 +0100

    Refactor as single class, that takes filesystem as input

--- deltalake/__init__.py
+++ deltalake/__init__.py
@@ -1,2 +1,2 @@
 __version__ = "0.1.0"
-from deltalake.reader import DeltaReader  # noqa: F401
+from deltalake.deltatable import DeltaTable  # noqa: F401
--- deltalake/deltatable.py
+++ deltalake/deltatable.py
@@ -3,25 +3,27 @@ import pyarrow.parquet as pq
 import re
 import json
 from copy import deepcopy
-from fsspec.registry import get_filesystem_class
+from fsspec.implementations.local import LocalFileSystem
 
 
 class DeltaTable:
-    def __init__(self, path, credential=None, account_name=None):
-        protocol, self.folder = path.split("://")
-        cls = get_filesystem_class(protocol=protocol)
-        self.fs = cls(account_name=account_name, credential=credential)
+    def __init__(self, path, file_system=None):
+
+        self.path = path
+        if file_system is None:
+            file_system = LocalFileSystem()
+        self.filesystem = file_system
         self._as_newest_version()
 
         # The PyArrow Dataset is exposed by a factory class,
         # which makes it hard to inherit from it directly.
         # Instead we will just have the dataset as an attribute and expose the important methods.
         self.pyarrow_dataset = pyarrow_dataset(
-            source=list(self.files), filesystem=self.fs
+            source=list(self.files), filesystem=self.filesystem
         )
 
     def _is_delta_table(self):
-        return self.fs.exists(f"{self.folder}/_delta_log/{0:020}.json")
+        return self.filesystem.exists(f"{self.path}/_delta_log/{0:020}.json")
 
     def _apply_from_checkpoint(self, checkpoint_version: int):
 
@@ -33,15 +35,15 @@ class DeltaTable:
             return
 
         # read latest checkpoint
-        with self.fs.open(
-            f"{self.folder}/_delta_log/{self.checkpoint:020}.checkpoint.parquet"
+        with self.filesystem.open(
+            f"{self.path}/_delta_log/{self.checkpoint:020}.checkpoint.parquet"
         ) as checkpoint_file:
             checkpoint = pq.read_table(checkpoint_file).to_pandas()
 
             for i, row in checkpoint.iterrows():
                 added_file = row["add"]["path"] if row["add"] else None
                 if added_file:
-                    self.files.add(f"{self.folder}/{added_file}")
+                    self.files.add(f"{self.path}/{added_file}")
 
     def _apply_partial_logs(self, version: int):
         # Checkpoints are created every 10 transactions,
@@ -50,8 +52,8 @@ class DeltaTable:
         # Effectively, this means that we can just create a
         # wild card for the first decimal of the checkpoint version
 
-        log_files = self.fs.glob(
-            f"{self.folder}/_delta_log/{self.checkpoint//10:019}*.json"
+        log_files = self.filesystem.glob(
+            f"{self.path}/_delta_log/{self.checkpoint//10:019}*.json"
         )
         # sort the log files, so we are sure we get the correct order
         log_files = sorted(log_files)
@@ -62,13 +64,13 @@ class DeltaTable:
             self.version = int(log_version)
 
             # Download log file
-            log = self.fs.cat(log_file)
+            log = self.filesystem.cat(log_file)
             for line in log.split():
                 meta_data = json.loads(line)
                 # Log contains other stuff, but we are only
                 # interested in the add or remove entries
                 if "add" in meta_data.keys():
-                    self.files.add(f"{self.folder}/{meta_data['add']['path']}")
+                    self.files.add(f"{self.path}/{meta_data['add']['path']}")
                 if "remove" in meta_data.keys():
                     remove_file = meta_data["remove"]["path"]
                     # To handle 0 checkpoints, we might read the log file with
@@ -76,7 +78,7 @@ class DeltaTable:
                     # remove a file that belongs to an ealier version,
                     # which we don't have in the list
                     if remove_file in self.files:
-                        self.files.remove(f"{self.folder}/{remove_file}")
+                        self.files.remove(f"{self.path}/{remove_file}")
             # Stop if we have reatched the desired version
             if self.version == version:
                 break
@@ -85,7 +87,9 @@ class DeltaTable:
         # Try to get the latest checkpoint info
         try:
             # get latest checkpoint version
-            checkpoint_info = self.fs.cat(f"{self.folder}/_delta_log/_last_checkpoint")
+            checkpoint_info = self.filesystem.cat(
+                f"{self.path}/_delta_log/_last_checkpoint"
+            )
             checkpoint_info = json.loads(checkpoint_info)
             self._apply_from_checkpoint(checkpoint_info["version"])
 
@@ -96,11 +100,21 @@ class DeltaTable:
         # we will just break when we don't find any newer logs
         self._apply_partial_logs(version=self.checkpoint + 9)
 
-    def to_table(self, **kwargs):
-        return self.pyarrow_dataset.to_table(**kwargs)
+    def to_table(self, columns=None, filter=None, batch_size=1e9, **kwargs):
+        """
+        https://arrow.apache.org/docs/python/generated/pyarrow.dataset.FileSystemDataset.html#pyarrow.dataset.FileSystemDataset.scan
+        """
+        return self.pyarrow_dataset.to_table(
+            columns=columns, filter=filter, batch_size=batch_size, **kwargs
+        )
 
-    def to_pandas(self):
-        return self.to_table().to_pandas()
+    def to_pandas(self, columns=None, filter=None, batch_size=1e9, **kwargs):
+        """
+        # https://arrow.apache.org/docs/python/generated/pyarrow.Table.html?highlight=to_pandas#pyarrow.Table.to_pandas
+        """
+        return self.to_table(
+            columns=columns, filter=filter, batch_size=batch_size
+        ).to_pandas(**kwargs)
 
     def as_version(self, version: int, inplace=True):
         """
@@ -126,7 +140,7 @@ class DeltaTable:
             self._apply_from_checkpoint(nearest_checkpoint)
             self._apply_partial_logs(version=version)
             self.pyarrow_dataset = pyarrow_dataset(
-                source=list(self.files), filesystem=self.fs
+                source=list(self.files), filesystem=self.filesystem
             )
             return self
 
