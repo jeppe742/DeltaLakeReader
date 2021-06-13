29bf7d27bbdb2e18c31af3ccff3bd6c7a412e376
jeppe johan waarkjær olsen <jeppe742@gmail.com>
2021-06-13 11:50:39 +0200

    Add support for schema evolution

--- deltalake/__init__.py
+++ deltalake/__init__.py
@@ -1,2 +1,2 @@
-__version__ = "0.2.1"
+__version__ = "0.2.5"
 from deltalake.deltatable import DeltaTable  # noqa: F401
--- tests/test_deltatable_aws.py
+++ tests/test_deltatable_aws.py
@@ -35,9 +35,9 @@ class DeltaReaderAppendTest(TestCase):
         )
 
         for i in range(12):
-            df.write.partitionBy("number2").format("delta").mode("append").save(
-                self.path
-            )
+            df.withColumn("id", col("id") + 1000 * i).write.partitionBy(
+                "number2"
+            ).format("delta").mode("append").save(self.path)
         self.fs = S3FileSystem()
 
         self.fs.upload(self.path, f"{AWS_BUCKET}/{self.path}", recursive=True)
@@ -67,11 +67,10 @@ class DeltaReaderAppendTest(TestCase):
 
         # compare dataframes. The index may not be the same order, so we ignore it
         assert_frame_equal(
-            df_pandas.sort_values("id").reset_index(drop=True),
-            df_spark.sort_values("id").reset_index(drop=True),
+            df_pandas.set_index("id"), df_spark.set_index("id"), check_like=True
         )
 
-    def test_version(self):
+    def test_version_no_checkpoint(self):
         # read the parquet files using pandas
         df_pandas = self.table.as_version(5, inplace=False).to_pandas()
         # read the table using spark
@@ -84,8 +83,23 @@ class DeltaReaderAppendTest(TestCase):
 
         # compare dataframes. The index may not be the same order, so we ignore it
         assert_frame_equal(
-            df_pandas.sort_values("id").reset_index(drop=True),
-            df_spark.sort_values("id").reset_index(drop=True),
+            df_pandas.set_index("id"), df_spark.set_index("id"), check_like=True
+        )
+
+    def test_version_checkpoint(self):
+        # read the parquet files using pandas
+        df_pandas = self.table.as_version(11, inplace=False).to_pandas()
+        # read the table using spark
+        df_spark = (
+            self.spark.read.format("delta")
+            .option("versionAsOf", 11)
+            .load(self.path)
+            .toPandas()
+        )
+
+        # compare dataframes. The index may not be the same order, so we ignore it
+        assert_frame_equal(
+            df_pandas.set_index("id"), df_spark.set_index("id"), check_like=True
         )
 
     def test_partitioning(self):
@@ -122,9 +136,9 @@ class DeltaReaderUpdateTest(TestCase):
         )
 
         for i in range(12):
-            df.write.partitionBy("number2").format("delta").mode("append").save(
-                self.path
-            )
+            df.withColumn("id", col("id") + 1000 * i).write.partitionBy(
+                "number2"
+            ).format("delta").mode("append").save(self.path)
         # Create a temp view of table, so we can update it
         self.spark.read.format("delta").load(self.path).createOrReplaceTempView(
             "table1"
@@ -159,11 +173,10 @@ class DeltaReaderUpdateTest(TestCase):
 
         # compare dataframes. The index may not be the same order, so we ignore it
         assert_frame_equal(
-            df_pandas.sort_values("id").reset_index(drop=True),
-            df_spark.sort_values("id").reset_index(drop=True),
+            df_pandas.set_index("id"), df_spark.set_index("id"), check_like=True
         )
 
-    def test_version(self):
+    def test_version_no_checkpoint(self):
         # read the parquet files using pandas
         df_pandas = self.table.as_version(5, inplace=False).to_pandas()
         # read the table using spark
@@ -176,13 +189,140 @@ class DeltaReaderUpdateTest(TestCase):
 
         # compare dataframes. The index may not be the same order, so we ignore it
         assert_frame_equal(
-            df_pandas.sort_values("id").reset_index(drop=True),
-            df_spark.sort_values("id").reset_index(drop=True),
+            df_pandas.set_index("id"), df_spark.set_index("id"), check_like=True
+        )
+
+    def test_version_checkpoint(self):
+        # read the parquet files using pandas
+        df_pandas = self.table.as_version(11, inplace=False).to_pandas()
+        # read the table using spark
+        df_spark = (
+            self.spark.read.format("delta")
+            .option("versionAsOf", 11)
+            .load(self.path)
+            .toPandas()
+        )
+
+        # compare dataframes. The index may not be the same order, so we ignore it
+        assert_frame_equal(
+            df_pandas.set_index("id"), df_spark.set_index("id"), check_like=True
         )
 
     def test_partitioning(self):
         # Partition pruning should half number of rows
         assert self.table.to_table(filter=ds.field("number2") == 0).num_rows == 6000
+
+    def test_predicate_pushdown(self):
+        # number is random 0-1, so we should have fewer than 12000 rows no matter what
+        assert self.table.to_table(filter=ds.field("number") < 0.5).num_rows < 12000
+
+    def test_column_pruning(self):
+        t = self.table.to_table(columns=["number", "number2"])
+        assert t.column_names == ["number", "number2"]
+
+
+class DeltaReaderSchemaEvolutionTest(TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.path = f"tests/{str(uuid.uuid4())}/table1"
+        self.spark = (
+            pyspark.sql.SparkSession.builder.appName("deltalake")
+            .config("spark.jars.packages", "io.delta:delta-core_2.12:0.7.0")
+            .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+            .config(
+                "spark.sql.catalog.spark_catalog",
+                "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+            )
+            .getOrCreate()
+        )
+
+        df = (
+            self.spark.range(0, 1000)
+            .withColumn("number", rand())
+            .withColumn("number2", when(col("id") < 500, 0).otherwise(1))
+        )
+
+        for i in range(5):
+            df.withColumn("id", col("id") + 1000 * i).write.partitionBy(
+                "number2"
+            ).format("delta").mode("append").save(self.path)
+
+        # Add data with one more column, using schema evolution
+        df = df.withColumn("number3", rand())
+        for i in range(5):
+            df.withColumn("id", col("id") + 1000 * (i + 5)).write.partitionBy(
+                "number2"
+            ).option("mergeSchema", "true").format("delta").mode("append").save(
+                self.path
+            )
+
+        # Add data with one more column, using schema evolution
+        df = df.withColumn("number4", rand())
+        for i in range(5):
+            df.withColumn("id", col("id") + 1000 * (i + 10)).write.partitionBy(
+                "number2"
+            ).option("mergeSchema", "true").format("delta").mode("append").save(
+                self.path
+            )
+
+        self.fs = S3FileSystem()
+
+        self.fs.upload(self.path, f"{AWS_BUCKET}/{self.path}", recursive=True)
+        self.table = DeltaTable(f"{AWS_BUCKET}/{self.path}", file_system=self.fs)
+
+    @classmethod
+    def tearDownClass(self):
+        # remove folder when we are done with the test
+        self.fs.rm(f"{AWS_BUCKET}/{self.path}", recursive=True)
+        shutil.rmtree(self.path)
+
+    def test_data(self):
+
+        # read the parquet files using pandas
+        df_pandas = self.table.to_pandas()
+        # read the table using spark
+        df_spark = self.spark.read.format("delta").load(self.table.path).toPandas()
+
+        # compare dataframes. The index may not be the same order, so we ignore it
+        assert_frame_equal(
+            df_pandas.set_index("id"), df_spark.set_index("id"), check_like=True
+        )
+
+    def test_version_no_checkpoint(self):
+        # read the parquet files using pandas
+        df_pandas = self.table.as_version(5, inplace=False).to_pandas()
+        # read the table using spark
+        df_spark = (
+            self.spark.read.format("delta")
+            .option("versionAsOf", 5)
+            .load(self.table.path)
+            .toPandas()
+        )
+
+        # compare dataframes. The index may not be the same order, so we ignore it
+        assert_frame_equal(
+            df_pandas.set_index("id"), df_spark.set_index("id"), check_like=True
+        )
+
+    def test_version_checkpoint(self):
+        # read the parquet files using pandas
+        df_pandas = self.table.as_version(11, inplace=False).to_pandas()
+        # read the table using spark
+        df_spark = (
+            self.spark.read.format("delta")
+            .option("versionAsOf", 11)
+            .load(self.table.path)
+            .toPandas()
+        )
+
+        # compare dataframes. The index may not be the same order, so we ignore it
+        assert_frame_equal(
+            df_pandas.set_index("id"), df_spark.set_index("id"), check_like=True
+        )
+
+    def test_partitioning(self):
+        # Partition pruning should half number of rows
+        assert self.table.to_table(filter=ds.field("number2") == 0).num_rows == 7500
 
     def test_predicate_pushdown(self):
         # number is random 0-1, so we should have fewer than 12000 rows no matter what
