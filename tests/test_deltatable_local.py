39ad75084f13e66cf41125b91cf48d9c880a20c5
Jeppe Johan Waarkjær Olsen <jwo@flsmidth.com>
2021-07-31 09:37:46 +0200

    bump package version

--- tests/test_deltatable_local.py
+++ tests/test_deltatable_local.py
@@ -11,7 +11,7 @@ from deltalake import DeltaTable, __version__
 
 
 def test_version():
-    assert __version__ == "0.2.6"
+    assert __version__ == "0.2.7"
 
 
 class DeltaReaderAppendTest(TestCase):
