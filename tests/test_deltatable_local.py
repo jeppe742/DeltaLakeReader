f2de2b736ad8ae5d1c3ff8e9d3d1975820fe9c3f
jeppe johan waarkjær olsen <jeppe742@gmail.com>
2022-03-10 10:29:17 +0100

    add failing test

--- tests/test_deltatable_local.py
+++ tests/test_deltatable_local.py
@@ -114,6 +114,78 @@ class DeltaReaderAppendTest(TestCase):
         assert t.column_names == ["number", "number2"]
 
 
+
+class DeltaReaderAppendTestNoCheckpoint(TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.path = f"tests/{str(uuid.uuid4())}/table1"
+        self.spark = (
+            pyspark.sql.SparkSession.builder.appName("deltalake")
+            .config("spark.jars.packages", "io.delta:delta-core_2.12:0.7.0")
+            .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+            .config(
+                "spark.sql.catalog.spark_catalog",
+                "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+            )
+            .getOrCreate()
+        )
+        df = (
+            self.spark.range(0, 1000)
+            .withColumn("number", rand())
+            .withColumn("number2", when(col("id") < 500, 0).otherwise(1))
+        )
+
+        for i in range(6):
+            df.withColumn("id", col("id") + 1000 * i).write.partitionBy(
+                "number2"
+            ).format("delta").mode("append").save(self.path)
+
+        self.table = DeltaTable(self.path)
+
+    @classmethod
+    def tearDownClass(self):
+        # remove folder when we are done with the test
+        shutil.rmtree(self.path)
+
+    def test_paths(self):
+        assert self.table.path == self.path
+        assert self.table.log_path == f"{self.path}/_delta_log"
+
+    def test_versions(self):
+
+        assert self.table.checkpoint == 0
+        assert self.table.version == 7
+
+    def test_data(self):
+
+        # read the parquet files using pandas
+        df_pandas = self.table.to_pandas()
+        # read the table using spark
+        df_spark = self.spark.read.format("delta").load(self.path).toPandas()
+
+        # compare dataframes. The index may not be the same order, so we ignore it
+        assert_frame_equal(
+            df_pandas.set_index("id"), df_spark.set_index("id"), check_like=True
+        )
+
+    def test_version_no_checkpoint(self):
+        # read the parquet files using pandas
+        df_pandas = self.table.as_version(4, inplace=False).to_pandas()
+        # read the table using spark
+        df_spark = (
+            self.spark.read.format("delta")
+            .option("versionAsOf", 4)
+            .load(self.path)
+            .toPandas()
+        )
+
+        # compare dataframes. The index may not be the same order, so we ignore it
+        assert_frame_equal(
+            df_pandas.sort_values("id").reset_index(drop=True),
+            df_spark.sort_values("id").reset_index(drop=True),
+        )
+
+
 class DeltaReaderUpdateTest(TestCase):
     @classmethod
     def setUpClass(self):
