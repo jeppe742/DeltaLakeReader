735f5028a3aa0e74ebf2d1fcd91600ebc9a3e10b
Jeppe Johan Waarkjær Olsen <jwo@flsmidth.com>
2022-03-10 13:59:29 +0100

    bump version

--- deltalake/__init__.py
+++ deltalake/__init__.py
@@ -1,2 +1,2 @@
-__version__ = "0.2.12"
+__version__ = "0.2.13"
 from deltalake.deltatable import DeltaTable  # noqa: F401
--- tests/test_deltatable_local.py
+++ tests/test_deltatable_local.py
@@ -12,7 +12,7 @@ from deltalake import DeltaTable, __version__
 
 
 def test_version():
-    assert __version__ == "0.2.12"
+    assert __version__ == "0.2.13"
 
 
 class DeltaReaderAppendTest(TestCase):
