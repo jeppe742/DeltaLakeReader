a5e52bbb166f4a054047c8fa5db191530fe01e0c
Jeppe Johan Waarkjær Olsen <jeppe742@gmail.com>
2023-11-22 20:27:48 +0100

    upgrade pyarrow version (#56)
    * upgrade pyarrow version
    
    * bump version

--- tests/test_deltatable_local.py
+++ tests/test_deltatable_local.py
@@ -12,7 +12,7 @@ from deltalake import DeltaTable, __version__
 
 
 def test_version():
-    assert __version__ == "0.2.15"
+    assert __version__ == "0.2.16"
 
 
 class DeltaReaderAppendTest(TestCase):
